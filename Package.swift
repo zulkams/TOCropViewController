280dae5a49b6ee8db4037f571f80e5acfb75c3d4
Zul Kamal <muhd.zulkamal@inglab.com.my>
2024-06-04 13:36:51 +0800

    update minimum version to 13.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "TOCropViewController",
     defaultLocalization: "en",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "TOCropViewController",
